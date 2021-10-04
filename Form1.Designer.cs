874e98e51a9eb861bfccc35ac19b7917a390d84b
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-10-04 11:53:57 -0400

    Added timer and horiz, vert speed.  Basic movement complete.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnimate));
+            this.imgBoat = new System.Windows.Forms.PictureBox();
+            this.tmrMoveBoat = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoat)).BeginInit();
             this.SuspendLayout();
+            // 
+            // imgBoat
+            // 
+            this.imgBoat.Image = ((System.Drawing.Image)(resources.GetObject("imgBoat.Image")));
+            this.imgBoat.Location = new System.Drawing.Point(125, 87);
+            this.imgBoat.Name = "imgBoat";
+            this.imgBoat.Size = new System.Drawing.Size(114, 65);
+            this.imgBoat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBoat.TabIndex = 0;
+            this.imgBoat.TabStop = false;
+            // 
+            // tmrMoveBoat
+            // 
+            this.tmrMoveBoat.Enabled = true;
+            this.tmrMoveBoat.Interval = 300;
+            this.tmrMoveBoat.Tick += new System.EventHandler(this.tmrMoveBoat_Tick);
             // 
             // FormAnimate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgBoat);
             this.Name = "FormAnimate";
             this.Text = "Animation";
+            this.Load += new System.EventHandler(this.FormAnimate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoat)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgBoat;
+        private System.Windows.Forms.Timer tmrMoveBoat;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,24 @@ namespace Animate_with_Timers
 {
     public partial class FormAnimate : Form
     {
+        int hSpeed, vSpeed;
         public FormAnimate()
         {
             InitializeComponent();
+        }
+
+        private void FormAnimate_Load(object sender, EventArgs e)
+        {
+            hSpeed = 5;
+            vSpeed = 5;
+        }
+
+        private void tmrMoveBoat_Tick(object sender, EventArgs e)
+        {
+            imgBoat.Left += hSpeed;
+            imgBoat.Top += vSpeed;
+
+
         }
     }
 }
