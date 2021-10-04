4fc204ddac56c9c9b77c29d2091b5c50b3392e38
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-10-04 11:48:42 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animate_with_Timers
+{
+    public partial class FormAnimate : Form
+    {
+        public FormAnimate()
+        {
+            InitializeComponent();
+        }
+    }
+}
